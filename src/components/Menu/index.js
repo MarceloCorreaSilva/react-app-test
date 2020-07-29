385b6657ef2bcf5925cd246536b52c3ba132794d
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-07-28 23:30:58 -0300

    Configurando Links (React Router Dom)

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Menu() {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <Link to="/" className="navbar-brand">Home</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <Link to="/sobre" className="nav-link">Sobre</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <Link to="/features" className="nav-link">Features</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <Link to="/pricing" className="nav-link">Pricing</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        <Link to="/disabled" className="nav-link disabled" tabindex="-1" aria-disabled="true">Disabled</Link>
                     </li>
                 </ul>
             </div>
