0c751f868361fb86b2b469fb09b4e3f2aa694544
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-07-29 21:13:00 -0300

    Criando e configurando Component Footer

--- src/pages/Layouts/Default/index.js
+++ src/pages/Layouts/Default/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Menu from '../../../components/Menu';
+import Footer from '../../../components/Footer';
 
 function Default({ children }) {
   return (
     <>
       <Menu />
       {children}
+      <Footer />
     </>
   );
 }
