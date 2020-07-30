4cbc732ff37ef5dfe88cc60856e2436a827830e0
Marcelo C. Silva <marcosmcorreasilva@gmail.com>
2020-07-29 22:50:53 -0300

    Atualizando pagina de erro / icon svg

--- src/pages/Error/NotFound/index.js
+++ src/pages/Error/NotFound/index.js
@@ -8,6 +8,11 @@ function NotFound() {
         <LayoutDefault>
             <center>
                 <h1>Página não encontrada!</h1>
+                <hr />
+                
+                <svg height="512" viewBox="0 0 64 64" width="512" xmlns="http://www.w3.org/2000/svg"><g id="Error_404" data-name="Error 404"><path d="m59 3h-54a2 2 0 0 0 -2 2v6h58v-6a2 2 0 0 0 -2-2z" fill="#9bc9ff" /><g fill="#1e81ce"><path d="m59 2h-54a3 3 0 0 0 -3 3v54a3 3 0 0 0 3 3h54a3 3 0 0 0 3-3v-54a3 3 0 0 0 -3-3zm-54 2h54a1 1 0 0 1 1 1v5h-56v-5a1 1 0 0 1 1-1zm54 56h-54a1 1 0 0 1 -1-1v-47h56v47a1 1 0 0 1 -1 1z" /><path d="m7 8h1a1 1 0 0 0 0-2h-1a1 1 0 0 0 0 2z" /><path d="m12 8h1a1 1 0 0 0 0-2h-1a1 1 0 0 0 0 2z" /><path d="m17 8h1a1 1 0 0 0 0-2h-1a1 1 0 0 0 0 2z" /><path d="m32 25a6.006 6.006 0 0 0 -6 6v10a6 6 0 0 0 12 0v-10a6.006 6.006 0 0 0 -6-6zm4 16a4 4 0 0 1 -8 0v-10a4 4 0 0 1 8 0z" /><path d="m55 39h-3v-13a1 1 0 0 0 -2 0v13h-5a1 1 0 0 1 -1-1v-12a1 1 0 0 0 -2 0v12a3 3 0 0 0 3 3h5v5a1 1 0 0 0 2 0v-5h3a1 1 0 0 0 0-2z" /><path d="m23 39h-3v-13a1 1 0 0 0 -2 0v13h-5a1 1 0 0 1 -1-1v-12a1 1 0 0 0 -2 0v12a3 3 0 0 0 3 3h5v5a1 1 0 0 0 2 0v-5h3a1 1 0 0 0 0-2z" /></g></g></svg>
+
+                <div>Icons made by <a href="https://www.flaticon.com/authors/srip" title="srip">srip</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             </center>
         </LayoutDefault>
     );
